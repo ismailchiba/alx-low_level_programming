bed5acc84099ebd64526d2c0f73fcfa39c52471b
ismailchiba <ismailchiba0@gmail.com>
2023-06-15 16:30:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /* 
  * using puts to print 
+ *
  * program return always 0
- * */
+ * 
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
