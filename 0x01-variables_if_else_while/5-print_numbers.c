47595933a8c6279701210900e7964b1894ca988e
ismailchiba <ismailchiba0@gmail.com>
2023-06-16 07:29:43 -0700

    5-print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 /**
  * main - entry point of the program,
- * description: prints numbers 0-10, followed by a new line
+ * description: prints numbers 0-9, followed by a new line
  * Return:Always 0 (Success)
  */
 int main(void)
 {
 	int m;
-	/*loop for to print numbers 0-10 */
-	for (m = 0; m <= 10; m++)
+	/*loop for to print numbers 0-9 */
+	for (m = 0; m < 10; m++)
 	{	printf("%d", m);
 	}
 	printf("\n");
