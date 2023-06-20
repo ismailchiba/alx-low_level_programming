5b3bc213cadd9f61b3eccba6d97fb6a34b269e77
ismailchiba <ismailchiba0@gmail.com>
2023-06-21 00:46:09 +0100

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,12 +4,11 @@
  *
  * parameter: c integer code ASCII
  * Return: 1 if c lowercase 0 otherwise
- *
  * 
  */
 int _islower(int c)
 {
-	if ( c > 96 && c < 123)
+	if (c > 96 && c < 123)
 		return (1);
 	else
 		return (0);
