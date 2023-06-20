acd9db58952d0436b15c1b5bbfb119d66c145892
ismailchiba <ismailchiba0@gmail.com>
2023-06-20 21:15:04 +0100

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+/*
+ * main- 
+ * description : use function to print _putchar
+ * Return : return 0
+ *
+ */
+void _putchar()
+{
+	printf("_putchar\n"); /*print _putchar*/
+}
+int main()
+{
+	_putchar();
+}
