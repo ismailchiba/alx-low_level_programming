65fd5aa18357091375b659714cc7ca6f1c6c6405
ismailchiba <ismailchiba0@gmail.com>
2023-06-21 01:28:01 +0100

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - the absolute value of an integer
+ * Return: integer abs of the value
+ **/
+int _abs(int c)
+{
+	if (c < 0)
+		return (-c);
+	else
+		return (c);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,6 +8,7 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int print_last_digit(int);
+int _abs(int c);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
