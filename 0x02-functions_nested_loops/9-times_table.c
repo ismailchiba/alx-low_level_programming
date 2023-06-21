623611468bfcb550f217711e8c9bc0e9bb6146df
ismailchiba <ismailchiba0@gmail.com>
2023-06-21 02:57:42 +0100

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,16 +6,17 @@
 void times_table(void)
 {
 	int a, b, c;
+
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b < 10; b++)
+		_putchar('0');
+		for (b = 1; b < 10; b++)
 		{
 			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
 			c = a * b;
 			_putchar(c + '0');
-			 
 		}
 		_putchar('$');
 		_putchar('\n');
