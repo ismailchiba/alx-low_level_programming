5f1bc8fd6b6f77f5c920307078d40568d7095631
ismailchiba <ismailchiba0@gmail.com>
2023-06-22 04:36:21 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,11 @@
 
 void print_numbers(void)
 {
-	int i;
+int i;
 
-	for (i = 48; i < 56; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+for (i = 48; i < 56; i++)
+{
+	_putchar(i);
+}
+_putchar('\n');
 }
