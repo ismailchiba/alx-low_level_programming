77b9b7f3b1ede317c5089612eda9e1a203aaa5a7
ismailchiba <ismailchiba0@gmail.com>
2023-06-22 05:14:19 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,13 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 48; j < 58; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j);
+			if (j > 9)
+			{
+				_putchar(1);
+			}
+			_putchar(j%10 + '0');
 		}
 		_putchar('\n');
 	}
