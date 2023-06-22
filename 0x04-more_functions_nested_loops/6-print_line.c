dfe0c40a88158890e7c3274e2f952c79b8453dfa
ismailchiba <ismailchiba0@gmail.com>
2023-06-22 05:33:47 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/*
+ * print_line - draws a straight line in the terminal
+ * @n : is the number of times the character _
+ *
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for ( i = 0; i <= n; i++)
+	{
+		if (n != 0)
+		{
+			_putchar(45);
+		}
+		else
+		{
+			_puchar('\n');
+			break;
+		}
+		_putchar('\n');
+	}
+}
