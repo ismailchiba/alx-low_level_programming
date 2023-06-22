47988e155c7bdfe6699f9d2b0ace10f35e5df9b3
ismailchiba <ismailchiba0@gmail.com>
2023-06-22 05:45:08 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,18 +8,17 @@
 
 void print_line(int n)
 {
-	int i;
-
-	for (i = 0; i <= n; i++)
+	if (n == 0)
 	{
-		if (n != 0)
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
 		{
 			_putchar('_');
-		}
-		else
-		{
-			_putchar('\n');
-			break;
 		}
 		_putchar('\n');
 	}
