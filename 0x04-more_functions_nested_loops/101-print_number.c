550b72ec40a26a045fd8926d555348f0e0f753cd
ismailchiba <ismailchiba0@gmail.com>
2023-06-22 16:53:05 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,21 +7,22 @@
 
 void print_number(int n)
 {
-	unsigned int i;
+unsigned int i;
 
-	if (n < 0)
-	{
-		i = -n;
-		_putchar('-');
-	}
-	else
-	{
-		i = n;
-	}
+if (n < 0)
+{
+	i = -n;
+	_putchar('-');
+}
+else
+{
+	i = n;
+}
 
-	if (i / 10)
-	{
-		print_number(i / 10);
-	}
-	_putchar((i % 10) + '0');
+if (i / 10)
+{
+	print_number(i / 10);
+}
+
+_putchar((i % 10) + '0');
 }
