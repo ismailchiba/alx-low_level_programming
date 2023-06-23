6b53466e179819b5897583bbeb53744af02cd8b8
ismailchiba <ismailchiba0@gmail.com>
2023-06-22 17:25:00 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	unsigned long int i, n = 612852475143;
+	long int i, n = 612852475143;
 
 	for(i = 3;i < sqrt(n); i=i+2)
 	{
 		while((n % i == 0) && n != i)
 			n = n / i;
 	}
-	printf("%ld", n);
+	printf("%ld\n", n);
 }
