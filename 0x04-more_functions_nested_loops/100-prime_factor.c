c536ef37f138e289a92d684675524ae15c051e55
ismailchiba <ismailchiba0@gmail.com>
2023-06-22 17:20:53 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 /*
  * main - prime factors
  * Return : 
  */
+int main(void)
+{
+	unsigned long int i, n = 612852475143;
 
+	for(i = 3;i < sqrt(n); i+=2)
+	{
+		while((n % i == 0) && n != i)
+			n = n / i;
+	}
+	printf("%ld", n);
+}
