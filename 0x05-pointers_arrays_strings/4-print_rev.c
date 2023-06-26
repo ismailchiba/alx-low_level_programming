64c2f3590528c33b77d37a0b550858688c3ffc74
ismailchiba <ismailchiba0@gmail.com>
2023-06-26 12:44:26 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - print in reverse
+ * @s : the string
+ * Description: prints a string, in reverse, followed by a new line.
+ */
+
+void print_rev(char *s)
+{
+	strrev(s);
+}
