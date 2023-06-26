229cf19fae293ae2651f4d3b0744b62a67563f56
ismailchiba <ismailchiba0@gmail.com>
2023-06-26 15:20:39 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,16 +4,17 @@
  * puts_half - array elements
  * @a:  array of integers
  * @n: n elements of an array
- * description: function that prints n elements of a array,
+ * Description: function that prints n elements of a array,
  *  followed by a new line
+ * Return: ...
  */
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n-1; i++)
+	for (i = 0; i < n - 1; i++)
 	{
 			printf("%d, ", a[i]);
 	}
-	printf("%d", a[n-1]);
+	printf("%d\n", a[n - 1]);
 }
