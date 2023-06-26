69abed8fb06bfe52cd6c8e1f349c480bf231d3c3
ismailchiba <ismailchiba0@gmail.com>
2023-06-26 12:28:19 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,7 +4,7 @@
  * _strlen - length of a string.
  * @s : the string
  * Description: used to calculate the length of a string
- * Return : the length value
+ * Return: the length value
  */
 
 int _strlen(char *s)
