ea5cb243affbf01208de6ee2eda6ba22448ff731
ismailchiba <ismailchiba0@gmail.com>
2023-06-26 14:47:30 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - prints half of a string
+ * @str : the string
+ * description : function that prints half of a string, followed by a new line
+ */
+
+void puts_half(char *str)
+{
+	int i, j;
+
+	j = strlen(str);
+
+	for (i = j/2; i < j; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
