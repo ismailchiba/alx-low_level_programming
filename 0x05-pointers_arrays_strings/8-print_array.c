bb8ab5576233e3578047c0c9219e924a959153e4
ismailchiba <ismailchiba0@gmail.com>
2023-06-26 15:17:07 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,15 +11,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n-1; i++)
 	{
-		if (i < n - 1)
-		{
 			printf("%d", a[i]);
 			_putchar(',');
 			_putchar(' ');
-		}
-		else
-			printf("%d", a[i]);
 	}
+	printf("%d", a[n-1]);
 }
