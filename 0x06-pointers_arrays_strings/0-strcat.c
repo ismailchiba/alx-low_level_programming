0073536162898d4bb2a3170bc78a15dbf903bf77
ismailchiba <ismailchiba0@gmail.com>
2023-06-29 16:40:35 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,27 @@
 #include "main.h"
 
-/**
+/*
  * _strcat - concatenates two strings
  * @dest: string
  * @src: string
- * Return : string src append to dest
+ *
+ * Return: string src append to dest
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i, k;
 
 	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	j = 0;
-	while (src[j] != '\0')
+	k = 0;
+	while (src[k] != '\0')
 	{
-		dest[i] = src[j];
+		dest[i] = src[k];
 		i++;
-		j++;
+		k++;
 	}
 
 	dest[i] = '\0';
