ef8b7f01f0e512ddac3394296acd3fd516801bef
ismailchiba <ismailchiba0@gmail.com>
2023-06-29 16:26:28 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,17 +8,21 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j, k;
+	int i, k;
 
-	for (i = 0; dest[i] < '\0'; i++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
+		i++;
 	}
-
-	for (j = i-1, k = 0; src[k] < '\0'; j++, k++)
+	k = 0;
+	while (src[k] != '\0')
 	{
-		dest[j] = src[k];
+		dest[i] = src[k];
+		i++;
+		k++;
 	}
-	dest[j] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
