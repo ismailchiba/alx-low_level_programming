a2ea3a29f4e25b7691bd7a9d09aed9b3b4e95a59
ismailchiba <ismailchiba0@gmail.com>
2023-06-29 18:03:43 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,12 +6,11 @@
  * @s2: second input 
  * Return: integer
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	for (i = 0; (s1[i] != '\0') && (s2[i] != '\0'); i++)
 	{
 		if (s1[i] != s2[i])
 		{
