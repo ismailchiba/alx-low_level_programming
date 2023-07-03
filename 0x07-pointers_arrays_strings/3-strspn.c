cb064883b6f1993a15642c03fb450c77fcdaf20c
ismailchiba <ismailchiba0@gmail.com>
2023-07-03 08:38:09 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,7 +7,18 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
-	
+	unsigned int i, j, p;
 
-
+	p = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				p++;
+			}
+		}
+	}
+	return (p);
+}
