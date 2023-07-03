aed589fb1c0073e09355d898aae74e291610db37
ismailchiba <ismailchiba0@gmail.com>
2023-07-03 09:15:25 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,21 +2,20 @@
 #include "main.h"
 
 /**
- * _strchr - function locates a character
+ * _strchr - Entry point
  * @s: input
  * @c: input
  * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
+	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
-		{
-			return (&s[i]);
-		}
+			return (s + i);
 	}
+
 	return (NULL);
 }
