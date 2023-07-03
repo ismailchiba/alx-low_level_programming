019fcf259152597c1486b48576a436105571e50d
ismailchiba <ismailchiba0@gmail.com>
2023-07-03 09:37:47 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *fir = haystack;//first
+		char *sec = needle;//second
+
+		while (*fir == *sec && *sec != '\0')
+		{
+			fir++;
+			sec++;
+		}
+
+		if (*sec == '\0')
+			return (haystack);
+	}
+
+	return (NULL);
+}
