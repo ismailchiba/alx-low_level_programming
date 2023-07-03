448822a49cd5610e283c07de0588075366e067fe
ismailchiba <ismailchiba0@gmail.com>
2023-07-03 08:23:08 -0700

    2-

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,11 +2,11 @@
 #include <stddef.h>
 
 /**
- * _strchr - function  that locates a character
- * @s: the address of memory
- * @c: the element searching for
- * Return: a pointer to the first occurrence of the character
- */
+ * _strchr - function locates a character
+ * @s : input
+ * @c : input
+ * Return : NULL
+ **/
 
 char *_strchr(char *s, char c)
 {
