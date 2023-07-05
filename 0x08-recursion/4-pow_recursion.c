57a9b7ff93a54d584f2953b39c335967b684784c
ismailchiba <ismailchiba0@gmail.com>
2023-07-05 11:01:39 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * pow_recursion - urns the value of x raised to the power of y
+ * _pow_recursion - returns the value of x raised to the power of y
  * @x: input value
  * @y: input value
  * Return: intger the power of x^y
