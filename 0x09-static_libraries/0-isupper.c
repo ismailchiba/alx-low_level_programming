d32f14180928e4b2408eeb96fb9c8c481d7144bb
root <root@DESKTOP-3D8FQDJ.localdomain>
2023-07-07 14:12:21 +0100

    0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  * _islower - checks for lowercase
+ *   * @c:  code ASCII character to be checked
+ *    * Return: 1 if c lowercase 0 otherwise
+ *     **/
+int _islower(int c)
+{
+		if (c > 96 && c < 123)
+					return (1);
+			else
+						return (0);
+}
