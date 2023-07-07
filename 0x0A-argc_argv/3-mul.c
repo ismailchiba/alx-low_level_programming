3eb9f8b8875301e82c5f71f90a1fdc9c23b645d7
ismailchiba <ismailchiba0@gmail.com>
2023-07-07 10:36:41 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,70 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s : char array
+ * Return: integer array
+ */
+
+int _atoi(char *s)
+{
+	int i, j, n, len, f, digit;
+	/**
+	 * i , j for incrementation
+	 * n the return value
+	 * len length of s
+	 **/
+
+	i = 0;
+	j = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++j;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (j % 2)
+				digit = -digit;
+			n = n * 10 + digit;/* store digit by digit */
+			f = 1;/* equals 1 if it is notan integer*/
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;/*the value of f is 1 break loop*/
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
+ * main - body
+ * @argc: input
+ * @argv: input
+ * Return: Always 0 success
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc  > 2)
+	{
+		printf("%d\n", _atoi(argv[1]) * _atoi(argv[2]));
+	}
+	else 
+		printf("Error\n");
+	return (0);
+}
