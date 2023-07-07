8b3b564c95b4b8e434040ce2948559811ef3aca6
ismailchiba <ismailchiba0@gmail.com>
2023-07-07 09:55:14 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - body
+ * @argc: input
+ * @argv: input
+ * Return: Always 0 success
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+	(void)argc;
+}
