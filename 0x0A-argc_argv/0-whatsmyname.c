cfa1436d73afbfced6deeb5554a90c3b590dc887
ismailchiba <ismailchiba0@gmail.com>
2023-07-07 09:31:13 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.c"
+
+/**
+ * main - body
+ * @argc: input
+ * @argv: input
+ * Return: always 0 success
+ */
+
+int main(int argc, char **argv)
+{
+	_putchar(argv[0]);
+	return (0);
+}
