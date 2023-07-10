21f716fd079187717f5e954e71dd2bbf7fef7d8f
ismailchiba <ismailchiba0@gmail.com>
2023-07-10 05:37:27 -0700

    1-strdup.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,23 +2,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/*
- * _strdup - returns a pointer to a new string which
+/**
+ * _strdup - returns a pointer to a new string which,
  *   is a duplicate of the string
  * @str: string
  * Return: NULL if str is NULL returns a pointer to the duplicated string
  */
-
 char *_strdup(char *str)
 {
 	char *array;
 	unsigned int i, j;
 
 	i = 0, j = 0;
-	if (str == NULL)
-        {
-                return (NULL);
-        }
+	if (*str == NULL)
+		return (NULL);
 	while (str[i] != '\0')
 	{
 		j++;
@@ -31,9 +28,7 @@ char *_strdup(char *str)
 		array[i] = str[i];
 	}
 	if (array == NULL)
-	{
 		return (NULL);
-	}
 	return (array);
 	free(array);
 }
