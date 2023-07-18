219980a7ea9024a2b9dec88e52b94445969a6758
ismailchiba <ismailchiba0@gmail.com>
2023-07-17 17:45:27 -0700

    filename

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/*
- * main - body
+
+/**
+ * main - body file name
  * Return: always 0 success
  */
 
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
