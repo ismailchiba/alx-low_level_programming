9458df14b7dabeb3e542654af38918d10c638733
ismailchiba <ismailchiba0@gmail.com>
2023-07-20 12:06:47 -0700

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - addition of all elements (args)
+ * @separator: separation caracter
+ * @n: number of elements
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	int i, j;
+
+	va_start(args, n);
+
+	i = (int)n;
+	for (j = 0; j < i; j++)
+	{
+		printf("%d", va_arg(args, int));
+		if (j < i - 1 && separator)
+			printf("%s ", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
