bf4bec55e52a837185c70bbf35f781ac802e7b25
ismailchiba <ismailchiba0@gmail.com>
2023-07-27 18:56:09 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,9 +18,10 @@ list_t *add_node(list_t **head, const char *str)
 		len++;
 	}
 
-	new_node = malloc(sizeof list_t);
+	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
+
 	new_node->len = (unsigned int)len;
 	new_node->str = strdup(str);
 	new_node->next = *head;
