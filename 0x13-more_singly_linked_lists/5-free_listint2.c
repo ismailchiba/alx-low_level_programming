e0bcdedce3e1b7463378aad8c67909e95378e0d2
ismailchiba <ismailchiba0@gmail.com>
2023-08-02 11:27:20 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,14 +9,15 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	listint_t *current = *head;
 
 	if (!head)
 		return;
-	while (head->next)
+	while (current->next)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		temp = current->next;
+		free(current);
+		current = temp;
 	}
 	free(temp);
 	*head = NULL;
