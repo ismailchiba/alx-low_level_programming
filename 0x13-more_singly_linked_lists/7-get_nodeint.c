24770fa72a1fb7e12900781c2536335a43cc0569
ismailchiba <ismailchiba0@gmail.com>
2023-08-02 12:41:24 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (!head || !index)
 		return (NULL);
-	while (current && n < index)
+	while (current && c < index)
 	{
 		current = current->next;
 		c++;
