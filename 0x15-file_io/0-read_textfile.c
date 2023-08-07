5384cee4352589092b619b911a3d084b8e143d2f
ismailchiba <ismailchiba0@gmail.com>
2023-08-07 14:28:18 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,34 +16,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t letters_written;
 
 	fd = open(filename, O_RDONLY);
-
 	if (filename == NULL || !letters)
 		return (0);
-
 	if (fd == -1)
 		return (0);
-
 	cont = malloc(letters + 1);/*continer*/
-
 	if (cont == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-
 	c_letters = read(fd, cont, letters);/*count bytes*/
-
 	if (c_letters <= 0)
 	{
 		close(fd);
 		free(cont);
 		return (0);
 	}
-
 	cont[c_letters] = '\0';/*Null terminate the string*/
-
 	letters_written = write(STDOUT_FLIENO, cont, c_letters);
-
 	if (letters_written != c_letters)
 	{
 		close(fd);
