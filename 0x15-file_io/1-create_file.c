9c8d661f740f1904a9d7386de95e542d50f0d0c9
ismailchiba <ismailchiba0@gmail.com>
2023-08-07 17:57:59 -0700

    2-append_text_to_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,27 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
- *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * create_file - function that creates a file
+ * @filename: file name
+ * @text_content: content
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int fd, wr, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
+		while (text_content[len])/*length*/
 			len++;
 	}
-
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	wr = write(fd, text_content, len);
 
-	if (fd == -1 || w == -1)
+	if (fd == -1 || wr == -1)/* Check for errors*/
 		return (-1);
-
 	close(fd);
-
 	return (1);
-}
