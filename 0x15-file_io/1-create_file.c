4bc3f00d585bec1d42ae19b6d7b5c8461f211f3b
ismailchiba <ismailchiba0@gmail.com>
2023-08-07 17:44:56 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,13 +6,13 @@
  * @text_content: content
  * Return: 1 on success, -1 on failure
  */
-
 int create_file(const char *filename, char *text_content)
 {
 	int fd, wr, len = 0;
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
 		while (text_content[len])/*length*/
