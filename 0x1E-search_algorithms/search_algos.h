1d50eac0fcbf94eabecb0ecf2873cfc24b67b1ec
root <root@DESKTOP-A0SGFGQ>
2024-07-18 02:29:08 +0100

    102-interpolation.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,4 +11,5 @@ int binary_search(int *array, size_t size, int value);
 int binary_search_index(int *array, size_t L_index, size_t R_index, int value);
 void print_array(int *array, size_t l, size_t r);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 #endif
